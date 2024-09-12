98e71eab1914142cee8767fe8e97fb6a0a4f637c
AliJafari <alisedghjafari@gmail.com>
2024-09-12 08:58:14 -0700

    f

--- components/mobileFooter/index.js
+++ components/mobileFooter/index.js
@@ -37,27 +37,29 @@ const icons = [
 ];
 
 export default function MobileFooter() {
+
+  const [notificationList, setNotificationList] = useState([]);
+  const {
+    data: notifications,
+    isLoading,
+    refetch,
+    isError,
+  } = useUserNotifications();
+
+  useEffect(() => {
+    if (notifications && !notifications?.detail) {
+      setNotificationList(notifications);
+    }
+  }, [notifications]);
+  console.log("noooo", notificationList);
   // const isIOS = () => {
   //   return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
   // };
   //${isIOS() ? "standalone:pb-[20px]" : ""}
   //pb-safe-bottom
 
-  const renderItem = () => {
-    const [notificationList, setNotificationList] = useState([]);
-    const {
-      data: notifications,
-      isLoading,
-      refetch,
-      isError,
-    } = useUserNotifications();
+  const renderItem = (notificationList) => {
 
-    useEffect(() => {
-      if (notifications && !notifications?.detail) {
-        setNotificationList(notifications);
-      }
-    }, [notifications]);
-    console.log("noooo", notificationList);
 
     return (
       <>
@@ -76,7 +78,7 @@ export default function MobileFooter() {
     <footer
       className={`padding-b-ios bg-white pt-1 px-2 w-full flex justify-around items-center fixed bottom-0 left-0 right-0 shadow-[0px_0px_31.4px_0px_rgba(4,6,15,0.15)] max-h-[83px] z-50`}
     >
-      {renderItem()}
+      {renderItem(notificationList)}
     </footer>
   );
 }
